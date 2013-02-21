650eacabffedf63e0ed055b9e340de770cce8c6e
Shahbaz Javeed <sjaveed@gmail.com>
2013-02-21 10:13:47 +0400

    * Fleshed out the Helper for CyberSourceSop a bit more so we have a bit more automation in place.  Time to test this out.

--- lib/active_merchant/billing/integrations/cyber_source_sop/helper.rb
+++ lib/active_merchant/billing/integrations/cyber_source_sop/helper.rb
@@ -8,6 +8,8 @@ module ActiveMerchant #:nodoc:
           mapping :currency, 'currency'
           mapping :amount,   'amount'
 
+          mapping :credential2 => 'orderPage_serialNumber'
+
           mapping :customer,
             :first_name => 'billTo_firstName',
             :last_name  => 'billTo_lastName',
@@ -41,6 +43,52 @@ module ActiveMerchant #:nodoc:
           mapping :notify_url, 'orderPage_merchantURLPostAddress'
           mapping :return_url, 'orderPage_receiptResponseURL'
           mapping :cancel_return_url, 'orderPage_cancelResponseURL'
+
+          # These are the options that need to be used with payment_service_for with the
+          # :cyber_source_sop service
+          #
+          # * :merchant_id => 'Your CyberSource SOP Merchant Id'
+          # * :shared_secret => 'Your CyberSource SOP Shared Secret'
+          # * :credential2 => 'Your CyberSource SOP Serial Number'
+          #
+          # The following are optional data that you can specify but will be set to sensible
+          # defaults if they're not specified
+          #
+          # * :transaction_type   default: 'sale', can be: 'sale', 'authorize'
+          #                       Determines the type of transaction this will be.  There's no concept of
+          #                       capture *after* an authorization so 'sale' will most likely work for you
+          # * :ignore_avs         default: 'true', can be: 'true', 'false'
+          #                       Whether or not to ignore the AVS code when processing this transaction
+          def initialize(order, account, options = {})
+            requires!(options, :credential2)
+            super
+
+            add_field('orderPage_transactionType', options[:transaction_type].present ? options[:transaction_type] : 'sale')
+            add_field('orderPage_ignoreAVS', options[:ignore_avs].present ? options[:ignore_avs] : 'true')
+            add_field('orderPage_version', options[:version].present ? options[:version] : '7')
+          end
+
+          def valid_line_item?(item = {})
+            item[:name].present? && item[:sku].present? && item[:unit_price].present?
+          end
+
+          def add_line_items(options = {})
+            requires!(options, :line_items)
+
+            valid_line_items = options[:line_items].select { |item| valid_line_item? item }
+            add_field('lineItemCount', valid_line_items.size)
+
+            valid_line_items.each_with_index do |item, idx|
+              tax_amount = (item[:tax_amount].present && item[:tax_amount] >= 0.0) ? item[:tax_amount] : '0.00'
+              quantity = item[:quantity].present ? item[:quantity] : 1
+
+              add_field("item_#{idx}_productName", item[:name])
+              add_field("item_#{idx}_productSKU", item[:sku])
+              add_field("item_#{idx}_taxAmount", tax_amount)
+              add_field("item_#{idx}_unitPrice", item[:unit_price])
+              add_field("item_#{idx}_quantity", quantity)
+            end
+          end
         end
       end
     end
